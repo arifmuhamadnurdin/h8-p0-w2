54026171f4c7175425a0ed5e7801d59d0437e39b
arifmuhamadnurdin <arifmuhamadnurdin25@gmail.com>
2019-11-19 23:14:27 +0700

    latihan 1 & 3

---
+++ exercise-3.js
@@ -0,0 +1,22 @@
+var nama = 'Nina'
+var peran = 'Ksatria'
+
+
+if (nama = 'Mikael') {
+    console.log('Halo Mikael, Pilih peranmu untuk memulai game!')
+}
+else if (nama = 'Nina') {
+    console.log('Selamat datang di Dunia Proxytia, Nina')
+    console.log('Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!')
+}
+else if (nama = 'Danu') {
+    console.log('Selamat datang di Dunia Proxytia, Danu')
+    console.log('Halo Tabib Danu, kamu akan membantu temanmu yang terluka!')
+}
+else if (nama = 'Zero') {
+    console.log('Selamat datang di Dunia Proxytia, Zero')
+    console.log('Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!')
+}
+//else (nama = '') {
+ //   console.log('Nama harus diisi')
+//}
