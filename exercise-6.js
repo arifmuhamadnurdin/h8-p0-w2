781cb6d5d753d85ad657807f9b9f8beaf063788f
arifmuhamadnurdin <arifmuhamadnurdin25@gmail.com>
2019-11-20 19:40:55 +0700

    exercise5

---
+++ exercise-6.js
@@ -0,0 +1,16 @@
+//1. Melakukan Looping Menggunakan While
+var i=2;
+console.log('LOOPING PERTAMA')
+while (i<=20)
+{
+  console.log(i + " I love coding")
+  i++
+}
+
+var i=20
+console.log('LOOPING KEDUA')
+while (i>1)
+{
+  console.log(i + " I will become fullstack developer");
+  i--
+}
