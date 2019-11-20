bd91ca0f8d9c75f19242fdc250aa64dec04e08bd
arifmuhamadnurdin <arifmuhamadnurdin25@gmail.com>
2019-11-20 16:09:06 +0700

    exercise3

--- exercise-3.js
+++ exercise-3.js
@@ -1,22 +1,22 @@
-var nama = 'Nina'
-var peran = 'Ksatria'
+var nama = 'Arif'
+var peran = ''
 
 
-if (nama = 'Mikael') {
-    console.log('Halo Mikael, Pilih peranmu untuk memulai game!')
+if (nama === '') {
+    console.log('Nama harus diisi!')
 }
-else if (nama = 'Nina') {
+else if (nama !== '' && peran =='') {
+    console.log('Halo ' + nama +', Pilih peranmu untuk memulai game!')
+}
+else if (nama === 'Nina' && peran === 'Ksatria') {
     console.log('Selamat datang di Dunia Proxytia, Nina')
     console.log('Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!')
 }
-else if (nama = 'Danu') {
+else if (nama === 'Danu' && peran === 'Tabib') {
     console.log('Selamat datang di Dunia Proxytia, Danu')
     console.log('Halo Tabib Danu, kamu akan membantu temanmu yang terluka!')
 }
-else if (nama = 'Zero') {
+else if (nama === 'Zero' && peran === 'Penyihir') {
     console.log('Selamat datang di Dunia Proxytia, Zero')
     console.log('Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!')
 }
-//else (nama = '') {
- //   console.log('Nama harus diisi')
-//}
