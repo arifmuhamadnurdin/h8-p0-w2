9a05ce5a8606428c86238c2e382bc3bdd965b0fe
arifmuhamadnurdin <arifmuhamadnurdin25@gmail.com>
2019-11-23 22:41:07 +0700

    updatee

---
+++ exercise-13.js
@@ -0,0 +1,11 @@
+function xo(str) {
+  // you can only write your code here!
+  if()
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
